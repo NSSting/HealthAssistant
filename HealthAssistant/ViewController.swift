014d14444922bbc71b6fbfac96f30a5bf7ad0b4b
ZQ <18838106381@163.com>
2017-10-20 10:34:19 +0800

    初始化项目版本

---
+++ HealthAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthAssistant
+//
+//  Created by ZQ on 2017/10/19.
+//  Copyright © 2017年 DaisyLiLiFamilies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
